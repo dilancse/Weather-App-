70b4b56b9aef13e4ef04951154c8b1089cf71612
Dilan Nuwantha <dilan.nuwantha@gmail.com>
2020-02-17 00:21:59 +0530

    Included weather item and list component with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar/top-bar.component';
+import { WeatherItemComponent } from './WeatherItem/weather-item/weather-item.component';
+import { WeatherListComponent } from './WeatherList/weather-list/weather-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    WeatherItemComponent,
+    WeatherListComponent
   ],
   imports: [
     BrowserModule,
