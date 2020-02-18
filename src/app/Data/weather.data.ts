d03f74d0741b795bdfc6e10cf2153a1c8f3359ba
Dilan Nuwantha <dilan.nuwantha@gmail.com>
2020-02-19 00:00:52 +0530

    remove dummy data

--- src/app/Data/weather.data.ts
+++ src/app/Data/weather.data.ts
@@ -1,16 +1,4 @@
 import { WeatherItem } from '../Models/weather-item';
 
-export const WEATHER_ITEMS: WeatherItem[] = [
-{
-  cityName: 'London',
-  info: 'Sunny',
-  temperature: 30
-},
-{
-  cityName: 'Melbourn',
-  info: 'Cloudy',
-  temperature: 19
-}
-
-];
+export const WEATHER_ITEMS: WeatherItem[] = [];
 
